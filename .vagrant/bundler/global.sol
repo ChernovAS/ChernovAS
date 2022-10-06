e066b3b798c8030d64415577fc169a435898dbcd
ChernovAS <alexandrchernov6@gmail.com>
2022-10-06 10:48:45 +0200

    basic Vagrant file

--- .vagrant/bundler/global.sol
+++
@@ -1,1 +0,0 @@
-{"dependencies":[["vagrant-hostmanager",["= 1.8.9"]],["log4r",[">= 0"]],["concurrent-ruby",["~> 1.0"]],["i18n",[">= 0"]],["micromachine",[">= 2","< 4"]],["vagrant-vbguest",["= 0.30.0"]]],"checksum":"3f76b83368332061827698b1da3ab902772d5720a6a9c0ba9e8a09379dcee743","vagrant_version":"2.3.1"}
